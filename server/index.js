7f1b487a0a67e8a72015ae4279692692bb69d935
its-me-ojas <crestfallenwizard@gmail.com>
2023-10-02 01:09:20 +0530

    start files

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose, { mongo } from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://Crestfallen:IwbtbvomforArshia@cluster0.h8ygkqs.mongodb.net/'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`Server is up and running on port : ${PORT}`))).catch(e => console.log(e))
+
